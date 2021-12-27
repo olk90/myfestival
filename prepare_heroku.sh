cc2c72643b9c8b8c80bb541166d49031c8ab7751
Oliver Köhler <olk.git@posteo.eu>
2021-12-27 12:34:09 +0100

    updated prepare_heroku.sh
    integrating postgres via script

--- prepare_heroku.sh
+++ prepare_heroku.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# add postgres
+heroku addons:add heroku-postgresql:hobby-dev
+
 # init environment variables
 heroku config:set PLATFORM=HEROKU
 heroku config:set FLASK_APP=myfestival.py
