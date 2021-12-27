b9bcf6f3d28fd024b86b2dad4eb5abeea05e8ffc
Oliver Köhler <olk.git@posteo.eu>
2021-12-27 19:44:02 +0100

    fix logging issue

--- app/__init__.py
+++ app/__init__.py
@@ -73,16 +73,17 @@ def create_app(config_class=Config):
             stream_handler = logging.StreamHandler()
             stream_handler.setLevel(logging.INFO)
             app.logger.addHandler(stream_handler)
-        elif not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/myfestival.log',
-                                           maxBytes=10 * 10 * 1024,
-                                           backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: '
-            '%(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/myfestival.log',
+                                               maxBytes=10 * 10 * 1024,
+                                               backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: '
+                '%(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('MyFestival startup')
