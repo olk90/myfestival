b78a9db1c3bbb7a875d448b80e84ce06da0c62bc
Oliver Köhler <olk.git@posteo.eu>
2022-06-27 21:04:21 +0200

    extended console output in cli.py

--- app/cli.py
+++ app/cli.py
@@ -15,10 +15,10 @@ def __setup_owner(username):
     users = session.query(User).all()
     if len(users) == 0:
         create_user(username=username, access_level=UserAccessLevel.OWNER)
-        print('User {} created.'.format(username)
-              + ' Change password immediately after first login!')
+        print("User {} created.".format(username)
+              + " Change password immediately after first login!")
     else:
-        print('Found {} user(s) in database. Skipped creation of owner!'
+        print("Found {} user(s) in database. Skipped creation of owner!"
               .format(len(users)))
     Path(Config.UPLOADED_PHOTOS_DEST).mkdir(parents=True, exist_ok=True)
 
@@ -30,30 +30,30 @@ def register(app):
         pass
 
     @translate.command()
-    @click.argument('lang')
+    @click.argument("lang")
     def init(lang):
         """Initialize a new language."""
-        if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-            raise RuntimeError('extract command failed')
+        if os.system("pybabel extract -F babel.cfg -k _l -o messages.pot ."):
+            raise RuntimeError("extract command failed")
         if os.system(
-                'pybabel init -i messages.pot -d app/translations -l ' + lang):
-            raise RuntimeError('init command failed')
-        os.remove('messages.pot')
+                "pybabel init -i messages.pot -d app/translations -l " + lang):
+            raise RuntimeError("init command failed")
+        os.remove("messages.pot")
 
     @translate.command()
     def update():
         """Update all languages."""
-        if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-            raise RuntimeError('extract command failed')
-        if os.system('pybabel update -i messages.pot -d app/translations'):
-            raise RuntimeError('update command failed')
-        os.remove('messages.pot')
+        if os.system("pybabel extract -F babel.cfg -k _l -o messages.pot ."):
+            raise RuntimeError("extract command failed")
+        if os.system("pybabel update -i messages.pot -d app/translations"):
+            raise RuntimeError("update command failed")
+        os.remove("messages.pot")
 
     @translate.command()
     def compile():
         """Compile all languages."""
-        if os.system('pybabel compile -d app/translations'):
-            raise RuntimeError('compile command failed')
+        if os.system("pybabel compile -d app/translations"):
+            raise RuntimeError("compile command failed")
 
     @app.cli.group()
     def install():
@@ -61,51 +61,51 @@ def register(app):
         pass
 
     @install.command()
-    @click.option('--username', default='admin')
+    @click.option("--username", default="admin")
     def admin(username):
         """Set up owner of the installation"""
-        print('Prepare creation of first user')
+        print("Prepare creation of first user")
         __setup_owner(username)
 
     @install.command()
     def masterdata():
         """Creates mandatory default data such as PKU"""
-        print('Prepare creation of packaging unit types')
+        print("Prepare creation of packaging unit types")
 
         pku = session.query(PackagingUnitType).all()
         if len(pku) == 0:
             create_pku()
         else:
-            print('Found {} types in database. Skipped creation of PKU!'
+            print("Found {} types in database. Skipped creation of PKU!"
                   .format(len(pku)))
 
     @install.command()
     def testdata():
         """Creates some data for manual tests"""
-        print('Prepare test data creation')
+        print("Prepare test data creation in database {}".format(Config.SQLALCHEMY_DATABASE_URI))
         users = session.query(User).all()
         if len(users) == 0:
-            print('Create users')
-            __setup_owner(username='Batman')
-            create_user(username='Wonderwoman',
+            print("Create users")
+            __setup_owner(username="Batman")
+            create_user(username="Wonderwoman",
                         access_level=UserAccessLevel.ADMIN)
-            create_user(username='Flash')
-            create_user(username='Aquaman')
+            create_user(username="Flash")
+            create_user(username="Aquaman")
 
-            print('Create festivals')
-            create_festival('Summerbreeze',
+            print("Create festivals")
+            create_festival("Summerbreeze",
                             start=date(2019, 8, 13),
                             end=date(2019, 8, 18))
-            create_festival('Bierfest',
+            create_festival("Bierfest",
                             start=date(2019, 3, 13),
                             end=date(2019, 3, 15))
-            create_festival('Wacken',
+            create_festival("Wacken",
                             start=date(2019, 8, 1),
                             end=date(2019, 8, 6))
 
             create_pku()
         else:
-            print('Found {} user(s) in database. Skipped testdata creation'
+            print("Found {} user(s) in database. Skipped testdata creation"
                   .format(len(users)))
 
     @app.cli.group()
@@ -113,40 +113,41 @@ def register(app):
         """Collection of helpers for managing the database"""
         pass
 
-    @postgres.command('delete')
+    @postgres.command("delete")
     def delete_tables():
         """Deletes entries from all tables without dropping them"""
         meta = db.metadata
         for table in reversed(meta.sorted_tables):
-            print('Clear table {}'.format(table))
+            print("Clear table {}".format(table))
             session.execute(table.delete())
         session.commit()
 
     @postgres.command()
     def drop():
         """Drops current schema"""
-        print('Drop all tables')
+        print("Drop all tables of {}".format(Config.SQLALCHEMY_DATABASE_URI))
         session.remove()
-        db.engine.execute('DROP TABLE IF EXISTS alembic_version')
+        db.engine.execute("DROP TABLE IF EXISTS alembic_version")
         db.drop_all()
 
     @postgres.command()
     def initschema():
         """Drops current schema and executes flask db upgrade"""
-        print('Drop all tables')
+        print("Initialize database schema for {}".format(Config.SQLALCHEMY_DATABASE_URI))
+        print("Drop all tables")
         session.remove()
-        db.engine.execute('DROP TABLE IF EXISTS alembic_version')
+        db.engine.execute("DROP TABLE IF EXISTS alembic_version")
         db.drop_all()
-        print('Delete old profile photos')
+        print("Delete old profile photos")
         avatars = Config.UPLOADED_PHOTOS_DEST
         try:
             shutil.rmtree(avatars)
         except OSError as e:
             print("Error: %s : %s" % (avatars, e.strerror))
-        print('Delete chronicle photos')
+        print("Delete chronicle photos")
         photos = Config.UPLOAD_PATH
         try:
             shutil.rmtree(photos)
         except OSError as e:
             print("Error: %s : %s" % (photos, e.strerror))
-        os.system('flask db upgrade')
+        os.system("flask db upgrade")
